a075016c7437dc7d6e3c2f70c37e2a7dd324a18f
c.misman <mismancelaleddin@gmail.com>
2023-06-18 15:17:12 +0300

    state machine added to soda order project

---
+++ src/main/java/com/order/service/sm/SodaOrderStateMachineConfig.java
@@ -0,0 +1,26 @@
+package com.order.service.sm;
+
+import com.order.service.domain.SodaOrderEventEnum;
+import com.order.service.domain.SodaOrderStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+public class SodaOrderStateMachineConfig extends StateMachineConfigurerAdapter<SodaOrderStatusEnum, SodaOrderEventEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<SodaOrderStatusEnum, SodaOrderEventEnum> states) throws Exception {
+        states.withStates().initial(SodaOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(SodaOrderStatusEnum.class))
+                .end(SodaOrderStatusEnum.PICKED_UP)
+                .end(SodaOrderStatusEnum.DELIVERED)
+                .end(SodaOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(SodaOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(SodaOrderStatusEnum.ALLOCATION_EXCEPTION);
+    }
+}
